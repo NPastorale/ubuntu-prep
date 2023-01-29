e2dc69d6d1c19ab9139c7bd2837a89adcc6b9630
NPastorale <npastorale@gmail.com>
2023-01-29 12:26:00 +0100

    Add several more programs

--- ubuntu-prep.sh
+++ ubuntu-prep.sh
@@ -5,12 +5,18 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 
-cat << EOF > /etc/sudoers.d/nahue
+cat <<EOF >/etc/sudoers.d/nahue
 nahue ALL=(ALL) NOPASSWD:ALL
 EOF
 
 sudo apt update
-sudo apt install -y curl git
+sudo apt install -y \
+        curl \
+        git \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
 
 curl -L "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" --output chrome.deb
 
@@ -19,10 +25,32 @@ curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x6
 sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
 sudo add-apt-repository -y ppa:phoerious/keepassxc
 
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+        sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+        sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+
 sudo apt update
-sudo apt install -y ./chrome.deb ./vscode.deb syncthing
+sudo apt install -y ./chrome.deb ./vscode.deb syncthing docker-ce docker-ce-cli containerd.io docker-compose-plugin kubectl gh
 
 rm ./chrome.deb ./vscode.deb
 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl >/dev/null
